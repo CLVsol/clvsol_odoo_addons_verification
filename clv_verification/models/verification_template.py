6603c47d0964787385d01ebbaf7475b46ee00221
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-07-04 15:07:08 -0300

    [IMP] clv_verification (IMP verification_template)

--- clv_verification/models/verification_template.py
+++ clv_verification/models/verification_template.py
@@ -16,34 +16,6 @@ class VerificationTemplate(models.Model):
         help='Verification Template Name'
     )
 
-    # verification_max_task = fields.Integer(
-    #     string='Max Task Registers'
-    # )
-
-    # verification_disable_identification = fields.Boolean(
-    #     string='Disable Identification'
-    # )
-
-    # verification_disable_check_missing = fields.Boolean(
-    #     string='Disable Check Missing'
-    # )
-
-    # verification_disable_inclusion = fields.Boolean(
-    #     string='Disable Inclusion'
-    # )
-
-    # verification_disable_verification = fields.Boolean(
-    #     string='Disable Verification'
-    # )
-
-    # verification_last_update_start = fields.Datetime(
-    #     string="Last Update (Start)"
-    # )
-
-    # verification_last_update_end = fields.Datetime(
-    #     string="Last Update (End)"
-    # )
-
     notes = fields.Text(string='Notes')
 
     date_inclusion = fields.Datetime(
@@ -56,16 +28,16 @@ class VerificationTemplate(models.Model):
         help="Model name of the object on which the verification method to be called is located, e.g. 'res.partner'"
     )
 
-    # method = fields.Char(
-    #     string='Method',
-    #     required=True,
-    #     help="Name of the method to be called when the verification job is processed."
-    # )
-
     action = fields.Char(
         string='Action',
         required=False,
         help="Name of the action used to process the verification."
+    )
+
+    action_args = fields.Char(
+        string='Action Arguments',
+        required=False,
+        help="List  of arguments(Python dictionary format) for the action."
     )
 
     active = fields.Boolean(string='Active', default=1)
