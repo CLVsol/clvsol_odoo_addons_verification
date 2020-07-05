f43950250c6fc66930f95ebda87fce19d58de18a
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-07-05 15:03:30 -0300

    [IMP] clv_verification (IMP wizard/verification_template_mass_edit)

--- clv_verification/wizard/verification_template_mass_edit.py
+++ clv_verification/wizard/verification_template_mass_edit.py
@@ -22,61 +22,6 @@ class VerificationTemplateMassEdit(models.TransientModel):
         default=_default_verification_template_ids
     )
 
-    verification_disable_identification = fields.Boolean(
-        string='Disable Identification'
-    )
-    verification_disable_identification_selection = fields.Selection(
-        [('set', 'Set'),
-         ('remove', 'Remove'),
-         ], string='Disable Identification:', default=False, readonly=False, required=False
-    )
-
-    verification_disable_check_missing = fields.Boolean(
-        string='Disable Check Missing'
-    )
-    verification_disable_check_missing_selection = fields.Selection(
-        [('set', 'Set'),
-         ('remove', 'Remove'),
-         ], string='Disable Check Missing:', default=False, readonly=False, required=False
-    )
-
-    verification_disable_inclusion = fields.Boolean(
-        string='Disable Inclusion'
-    )
-    verification_disable_inclusion_selection = fields.Selection(
-        [('set', 'Set'),
-         ('remove', 'Remove'),
-         ], string='Disable Inclusion:', default=False, readonly=False, required=False
-    )
-
-    verification_disable_verification = fields.Boolean(
-        string='Disable Verification'
-    )
-    verification_disable_verification_selection = fields.Selection(
-        [('set', 'Set'),
-         ('remove', 'Remove'),
-         ], string='Disable Verification:', default=False, readonly=False, required=False
-    )
-
-    verification_last_update_start = fields.Datetime(
-        string="Last Update (Start)"
-    )
-    verification_last_update_start_selection = fields.Selection(
-        [('set', 'Set'),
-         ('remove', 'Remove'),
-         ], string='Last Update (Start):', default=False, readonly=False, required=False
-    )
-
-    verification_last_update_end = fields.Datetime(
-        string="Last Update (End)"
-    )
-    verification_last_update_end_selection = fields.Selection(
-        [('set', 'Set'),
-         ('remove', 'Remove'),
-         ], string='Last Update (End):', default=False, readonly=False, required=False
-    )
-
-    # @api.multi
     def _reopen_form(self):
         self.ensure_one()
         action = {
@@ -89,42 +34,11 @@ class VerificationTemplateMassEdit(models.TransientModel):
         }
         return action
 
-    # @api.multi
     def do_verification_template_mass_edit(self):
         self.ensure_one()
 
         for verification_template in self.verification_template_ids:
 
             _logger.info(u'%s %s', '>>>>>', verification_template.name)
-
-            if self.verification_disable_identification_selection == 'set':
-                verification_template.verification_disable_identification = self.verification_disable_identification
-            if self.verification_disable_identification_selection == 'remove':
-                verification_template.verification_disable_identification = False
-
-            if self.verification_disable_check_missing_selection == 'set':
-                verification_template.verification_disable_check_missing = self.verification_disable_check_missing
-            if self.verification_disable_check_missing_selection == 'remove':
-                verification_template.verification_disable_check_missing = False
-
-            if self.verification_disable_inclusion_selection == 'set':
-                verification_template.verification_disable_inclusion = self.verification_disable_inclusion
-            if self.verification_disable_inclusion_selection == 'remove':
-                verification_template.verification_disable_inclusion = False
-
-            if self.verification_disable_verification_selection == 'set':
-                verification_template.verification_disable_verification = self.verification_disable_verification
-            if self.verification_disable_verification_selection == 'remove':
-                verification_template.verification_disable_verification = False
-
-            if self.verification_last_update_start_selection == 'set':
-                verification_template.verification_last_update_start = self.verification_last_update_start
-            if self.verification_last_update_start_selection == 'remove':
-                verification_template.verification_last_update_start = False
-
-            if self.verification_last_update_end_selection == 'set':
-                verification_template.verification_last_update_end = self.verification_last_update_end
-            if self.verification_last_update_end_selection == 'remove':
-                verification_template.verification_last_update_end = False
 
         return True
