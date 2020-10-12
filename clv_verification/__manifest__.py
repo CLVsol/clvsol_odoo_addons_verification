2dff0854bf547df5fe2f2e2dc239fed82009e830
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2020-10-12 13:44:07 -0300

    [REF] clv_verification

--- clv_verification/__manifest__.py
+++ clv_verification/__manifest__.py
@@ -41,6 +41,7 @@
         'wizard/verification_batch_exec_view.xml',
         # # 'wizard/verification_batch_exec_view.xml',
         # 'wizard/verification_outcome_refresh_view.xml',
+        'views/verification_menu_view.xml',
         'data/global_log_client.xml',
     ],
     'demo': [],
